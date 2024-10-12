fab9a068f66928e65dc32850c8270b449476e185
LuguinTbX <126295743+LuguinTbX@users.noreply.github.com>
2024-10-12 00:19:47 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -3,64 +3,66 @@
 const labelElement = document.querySelector("#rbx-running-games > div.container-header > div.server-list-options > div.checkbox > label");
 
 try {
-    let todoTexto = document.body.innerText;
     let intervalo;
 
-
     function verificarUnk(texto) {
-        let inputElement = document.querySelector("#rbx-running-games > div.btr-pager-holder.btr-server-pager > ul > li.btr-pager-mid > input");
+        const inputElement = document.querySelector("#rbx-running-games > div.btr-pager-holder.btr-server-pager > ul > li.btr-pager-mid > input");
         if (texto.includes("Unknown") && inputElement) {
-            let inputValue = inputElement.value;
-            alert("Sv Br encontrado! Page: " + inputValue);  
+            const inputValue = inputElement.value;
+            alert("Sv Br encontrado! Página: " + inputValue);
             clearInterval(intervalo);  
         } else {
+            const nextButton = document.querySelector("#rbx-running-games > div.btr-pager-holder.btr-server-pager > ul > li.btr-pager-next > button");
+            if (nextButton) {
+                nextButton.click();
+            }
             console.log("Nada encontrado.");
         }
     }
 
-
-    function checkExtension(extensionId) {
-        const link = document.createElement('link');
-        link.href = `chrome-extension://${extensionId}/css/main.css`;  
-        link.rel = 'stylesheet';
-        link.onload = function() {
-
-            intervalo = setInterval(function() {
-                let textoAtualizado = document.body.innerText;
-                verificarUnk(textoAtualizado);
-            }, 1000);
-        };
-        link.onerror = function() {
-            alert("Extensão não instalada."); 
-        };
-        document.head.appendChild(link);
+    function iniciarVerificacao() {
+        intervalo = setInterval(() => {
+            const textoAtualizado = document.body.innerText;
+            verificarUnk(textoAtualizado);
+        }, 1000);
     }
 
-   
+    function carregarEstiloExtensao(extensionId) {
+        return new Promise((resolve, reject) => {
+            const link = document.createElement('link');
+            link.href = `chrome-extension://${extensionId}/css/main.css`;  
+            link.rel = 'stylesheet';
+            link.onload = resolve;
+            link.onerror = () => reject(new Error("Extensão não instalada."));
+            document.head.appendChild(link);
+        });
+    }
+
+    function configurarCheckbox() {
+        if (labelElement && !labelElement.checked) {
+            labelElement.click();
+        }
+    }
+
+    async function iniciarScript() {
+        try {
+            await carregarEstiloExtensao('hbkpclpemjeibhioopcebchdmohaieln');
+            iniciarVerificacao();
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     if (sessionStorage.getItem('reloadAfterError') === 'true') {
-        sessionStorage.removeItem('reloadAfterError'); 
-
-        
-        if (labelElement && !labelElement.checked) {
-            labelElement.click();
-        }
-
-     
-        checkExtension('hbkpclpemjeibhioopcebchdmohaieln'); 
-
-    } else {
-       
-        if (labelElement && !labelElement.checked) {
-            labelElement.click();
-        }
-
-    
-        checkExtension('hbkpclpemjeibhioopcebchdmohaieln');
+        sessionStorage.removeItem('reloadAfterError');
+        configurarCheckbox();
     }
+
+    configurarCheckbox();
+    iniciarScript();
 
 } catch (error) {
-
     alert("Ocorreu um erro, recarregando a página...");
-    sessionStorage.setItem('reloadAfterError', 'true');  
+    sessionStorage.setItem('reloadAfterError', 'true');
     location.reload(); 
 }
